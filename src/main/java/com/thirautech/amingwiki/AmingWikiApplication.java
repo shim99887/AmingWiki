8341dd218ded07bd1be2900b2d537722373a53f0
김영록 <shim99887@thirautech.com>
2022-03-14 09:22:01 +0900

    AmingWiki init

---
+++ src/main/java/com/thirautech/amingwiki/AmingWikiApplication.java
@@ -0,0 +1,13 @@
+package com.thirautech.amingwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmingWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmingWikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thirautech/amingwiki/AmingWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thirautech.amingwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmingWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
